f778515ee974f9ce246c34c5fd996b39f547e31e
diegodog2312 <54220428+diegodog2312@users.noreply.github.com>
2020-06-10 00:01:35 -0500

    Node.h
    Ya todo está funcionando bien, si notan algo raro me avisan.
    Love you <3

--- node.h
+++ node.h
@@ -4,43 +4,30 @@
 #include <stdbool.h>
 
 typedef struct _libro libro;
-
 struct _libro{
     char titulo[50];
     char autor[50];
     char editorial[50];  
     int ISBN;
-    bool formato;  //tapa blanda = false     tapa dura = true
+    bool formato;  //tapa blanda = false / tapa dura = true
     int existencia;
     int precio;
 };
 
-libro *crear_libro(char titulo[], char autor[], char editorial[], int ISBN, bool formato, int existencia, int precio);
-bool borrar_libro(libro *l); 
+//Funcion para crear un libro
+libro crear_libro(char titulo[], char autor[], char editorial[], int ISBN, bool formato, int existencia, int precio);
 
 typedef struct _dnodo dnodo;
-
 struct _dnodo{
-
-    libro *libro;
+    libro libro;
     dnodo *sig;
     dnodo *prev;
 };
 
-dnodo *crear_dnodo(libro *l);
+dnodo *crear_dnodo(libro l);
 bool borrar_dnodo(dnodo *n);
 
-typedef struct _nodo nodo;
-
-struct _nodo{
-
-    libro *libro;
-    nodo *sig;
-};
-
-nodo *crear_nodo(libro *l);
-bool borrar_nodo(nodo *n);
-
 #endif
-//Daniel 02-06-2020
-//Mara 04/06/2020 estantexnodo
+//Daniel 02/06/2020
+//Mara 04/06/2020 
+//Diego 06/06/2020
