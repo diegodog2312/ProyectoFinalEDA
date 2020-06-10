15b6975832dd39b9e1cbafef76dccbe6d8c5ea8f
diegodog2312 <54220428+diegodog2312@users.noreply.github.com>
2020-06-09 23:59:55 -0500

    Node.c
    Ya todo está funcionando, si notan algo raro me avisan.
    Love u <3

--- node.c
+++ node.c
@@ -1,31 +1,22 @@
-#include "nodo.h"
+#include "node.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-
-libro *crear_libro(char titulo[], char autor[], char editorial[], int ISBN, bool formato, int existencia, int precio){
-    libro *b;
-	b = (libro*)malloc(sizeof(libro));
-    strcpy(b->titulo, titulo);
-    strcpy(b->autor, autor);
-    strcpy(b->editorial, editorial);
-    b->ISBN = ISBN;
-    b->formato = formato;
-    b->existencia = existencia;
-    b->precio = precio;
+//Crea un libro y lo retorna
+libro crear_libro(char titulo[], char autor[], char editorial[], int ISBN, bool formato, int existencia, int precio){
+    libro b;
+    strcpy(b.titulo, titulo);
+    strcpy(b.autor, autor);
+    strcpy(b.editorial, editorial);
+    b.ISBN = ISBN;
+    b.formato = formato;
+    b.existencia = existencia;
+    b.precio = precio;
 	return b;
 }
 
-nodo *crear_nodo(libro *l){
-    nodo *e;
-    e = (nodo*)malloc(sizeof(nodo));
-    e->libro = l;
-    e->sig = NULL;
-    return e;
-}
-
-dnodo *crear_dnodo(libro *l){
+dnodo *crear_dnodo(libro l){
     dnodo *e;
     e = (dnodo*)malloc(sizeof(dnodo));
     e->libro = l;
@@ -34,29 +25,14 @@ dnodo *crear_dnodo(libro *l){
     return e;
 }
 
-
-
 bool borrar_dnodo(dnodo *e){
-    if (e->sig ==  e->prev){
-	    borrar_libro(e->libro);
+    if (e->sig ==  e->prev){	    
 		free(e);
         return true;
     }
     return false;
 }
 
-bool borrar_nodo(nodo *e){
-    if (e->sig == NULL){
-    	borrar_libro(e->libro);
-	    free(e);
-        return true;
-    }
-    return false;
-}
-
-bool borrar_libro(libro *l){
-	free(l);
-	return true;
-}
 //Daniel   02-06-2020
 //Mara 04/06/2020
+//Diego 06/06/2020
